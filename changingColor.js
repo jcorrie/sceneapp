4d8bd5da8f043f840a379c2c942c22c64bacd284
jcorrie <jonnycorrie@gmail.com>
2016-11-03 23:06:10 +0000

    Add files via upload

--- changingColor.js
+++ changingColor.js
@@ -3,13 +3,50 @@
             
             $(document).ready (function() {
                 
+                
+                $('p').addClass("mdl-color-text--grey-800");
+                $('h4').addClass("mdl-color-text--grey-900"); 
+              $('h5').addClass("mdl-color-text--grey-600"); 
+                
+                
+                $(".likeButton").click(function(){
+                  
+                  if($(this).find('.likeIcon').hasClass("mdl-color-text--blue-grey-400")){
+                  
+                 $(this).find('.likeIcon').removeClass("mdl-color-text--blue-grey-400"); 
+                 $(this).find('.likeIcon').addClass("mdl-color-text--blue-400"); 
+                 console.log("working...?");
+                 
+                  } else {
                     
+                    $(this).find('.likeIcon').addClass("mdl-color-text--blue-grey-400");
+                    $(this).find('.likeIcon').removeClass("mdl-color-text--blue-400"); 
+                  }
+                  
+                });
+
+
+                $("a").click(function(){
+                  
+                  if($(this).find('.starIcon').hasClass("mdl-color-text--grey-400")){
+                  
+                 $(this).find('.starIcon').removeClass("mdl-color-text--grey-400"); 
+                 $(this).find('.starIcon').addClass("mdl-color-text--pink-500"); 
+                 console.log("working...?");
+                 
+                  } else {
+                    
+                    $(this).find('.starIcon').addClass("mdl-color-text--grey-400");
+                    $(this).find('.starIcon').removeClass("mdl-color-text--pink-500"); 
+                  }
+                  
+                });
                   
                 
                 var keepThis = $("#availableTag").css("color");
                 
                 
-                
+                $(".cardIcon").addClass("mdl-color-text--blue-grey-400");
 
                 
                 
@@ -71,7 +108,7 @@
                           makePink();
                           $("#availableText").fadeTo( "slow", 0.87);
                         
-                        };
+                        }
      
                     
                         
@@ -84,14 +121,14 @@
                   $(myElement).addClass("mdl-color-text--blue-grey-400");
                   $(myElement).removeClass("mdl-color-text--pink-A400");
                   
-                };
+                }
                 
                   function makePink(){
                     
                     $(myElement).removeClass("mdl-color-text--blue-grey-400");
                     $(myElement).addClass("mdl-color-text--pink-A400");
                     
-                  };
+                  }
                   
                   
                 
